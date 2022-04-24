63cbbc00c8f800095696dbaaa761afa1d0e8b7c3
dfinke <finked@hotmail.com>
2022-04-24 09:40:50 -0400

    add example for column coloring

---
+++ ConditionalFormatting/Example2.ps1
@@ -0,0 +1,45 @@
+. .\spikeColumnColors.ps1
+
+$data = ConvertFrom-Csv @"
+A,B,C,D
+14,5,20,14
+4,2,20,3
+5,54,7,8
+4,3,3,2
+1,2,8,6
+"@
+
+$blueAndRed = {
+    param($targetData)
+    # $color = $PSStyle.Reverse
+    $color = $PSStyle.Background.Blue
+    if ([int]$targetData -lt 6) {
+        $color = $PSStyle.Background.Red
+    }
+    $color
+}
+
+function test {
+    param($targetData)
+    $color = $PSStyle.Reverse
+    if ([int]$targetData -lt 6) {
+        $color = $PSStyle.Background.Green
+    }
+    $color
+}
+
+''
+doColumnStyles $data @{
+    # A = { $PSStyle.Background.Green }  
+    B = { $PSStyle.Background.Yellow } 
+    C = { $PSStyle.Background.Yellow } 
+    # D = { $PSStyle.Background.Green }
+}
+''
+doColumnStyles $data @{
+    # A = $blueAndRed
+    B = $blueAndRed
+    C = $blueAndRed 
+    #D = (Get-Command test)
+    # D = $blueAndRed
+}
